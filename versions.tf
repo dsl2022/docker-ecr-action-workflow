573bcb1f1b62f0e650e3d0c7f912d579ea7c6de6
dsl2022 <2015rpro@gmail.com>
2023-12-14 23:02:25 -0500

    updated workflow

--- versions.tf
+++ versions.tf
@@ -8,5 +8,12 @@ terraform {
     }
   }
 
-  backend "s3" {}
+  backend "s3" {
+    bucket               = var.bucket # The S3 bucket where the Terraform state file will be stored
+    key                  = "./terraform.tfstate" # The path to the state file within the bucket
+    region               = var.region # The AWS region where the bucket is located
+    encrypt              = true # If the state should be encrypted
+    dynamodb_table       = var.dynamodb_table # DynamoDB table for state locking and consistency checking
+    role_arn             = var.role_arn # IAM role ARN for Terraform to assume, if necessary
+  }
 }
