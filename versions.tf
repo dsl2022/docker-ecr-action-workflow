b56feb579074978a9cd9a5458e7bc517d8e0df56
dsl2022 <2015rpro@gmail.com>
2023-12-15 00:18:53 -0500

    updated workflow

--- versions.tf
+++ versions.tf
@@ -14,18 +14,13 @@ terraform {
     region               = var.region # The AWS region where the bucket is located
     encrypt              = true # If the state should be encrypted
     dynamodb_table       = var.dynamodb_table # DynamoDB table for state locking and consistency checking
-    assume_role {
-    role_arn = "arn:aws:iam::212612999379:role/github-actions-dsl"
-    session_name = "terraform"
-  }
+    assume_role_with_web_identity = {
+    role_arn           = var.role_arn
+    }
   }
 }
 
 provider "aws" {
   profile = "github-action"
   region = "us-east-1"
-  assume_role {
-    role_arn = "arn:aws:iam::212612999379:role/github-actions-dsl"
-    session_name = "terraform"
-  }
 }
