c58275d2235cfca581070b5bc035cdafdcca5018
dsl2022 <2015rpro@gmail.com>
2023-12-15 09:31:14 -0500

    updated workflow

--- versions.tf
+++ versions.tf
@@ -4,18 +4,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.61.0"
+      version = ">= 4.22.0"
     }
   }
 
-  backend "s3" {
-    bucket               = var.bucket # The S3 bucket where the Terraform state file will be stored
-    key                  = "./terraform.tfstate" # The path to the state file within the bucket
-    region               = var.region # The AWS region where the bucket is located
-    encrypt              = true # If the state should be encrypted
-    dynamodb_table       = var.dynamodb_table # DynamoDB table for state locking and consistency checking
-    assume_role = {
-      role_arn = "arn:aws:iam::212612999379:role/github-actions-dsl"
-    }
-  }
+  backend "s3" {}
 }
